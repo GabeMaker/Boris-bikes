87844875d7ba9dd8635a0812a9f66886db7474f7
gabe <GabeMaker@users.noreply.github.com>
2014-12-10 17:29:55 +0000

    refactored tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,9 @@ require 'docking_station'
 describe DockingStation do
 	let(:bike) { Bike.new}
 	let(:station) { DockingStation.new(:capacity => 20)}
+	def fill_station(station)
+		20.times { station.dock(Bike.new) }
+	end	
 
 	it 'should accept a bike' do
 		# bike = Bike.new
@@ -19,11 +22,11 @@ describe DockingStation do
 
 	it 'should know when it\'s full' do
 		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
+		fill_station(station)
 		expect(station).to be_full
 	end
 	it 'should not accept a bike if its full' do
-		20.times { station.dock(Bike.new) }
+		fill_station(station)
 		expect(lambda { station.dock(bike)}).to raise_error(RuntimeError, 'Station is full')
 		# {} like a bomb disposal box, lambda not necessary but a keyword in Ruby
 	end
