d7b439ac22843dcb13876fbfd68c01015f490055
gabe <GabeMaker@users.noreply.github.com>
2014-12-10 13:15:29 +0000

    refactored tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,21 +4,20 @@ require 'bike'
 describe Bike do
 	# this is a specific feature
 	# that we expect to be present
+	let(:bike) { Bike.new}
+
 	it "should not be broken after we create it" do
-		the_bike = Bike.new #initialise a new object of Bike class
 		# expect an instance of this class to have
 		# a method "broken?" that should return false
-		expect(the_bike).not_to be_broken
+		expect(bike).not_to be_broken
 	end
 
 	it 'should be able to break' do
-		bike = Bike.new
 		bike.break!
 		expect(bike.broken?).to be true
 	end
 
 	it 'should be able to get fixed' do
-		bike = Bike.new
 		bike.break!
 		bike.fix!
 		expect(bike).not_to be_broken
